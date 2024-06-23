7c9d74aba3d9fcca24d9880af148a0a4a4c822c3
Tommaso Cesarini <cesarinitommaso@gmail.com>
2024-06-23 17:54:05 +0200

    Eliminated useless parts

--- src/lidar_remap.cpp
+++ src/lidar_remap.cpp
@@ -12,29 +12,22 @@ public:
     {
 
         pub = n.advertise<sensor_msgs::PointCloud2>("pointcloud_remapped", 1000);
-        f = boost::bind(&lidar_remap::paramCallback, this, _1, _2);
-        server.setCallback(f);
         sub = n.subscribe<sensor_msgs::PointCloud2>("/ugv/rslidar_points", 1, &lidar_remap::subCallback, this);
     }
 
-    void paramCallback(second_project::parametersConfig &config, uint32_t level)
-    {
-        odom_source = config.odom_source;
-    }
 
     void subCallback(const sensor_msgs::PointCloud2::ConstPtr &msg)
     {
         sensor_msgs::PointCloud2 modifiedMsg = *msg;
-        modifiedMsg.header.frame_id = odom_source;
+        modifiedMsg.header.frame_id = "UGV_odom";
         modifiedMsg.header.stamp = msg->header.stamp;
         pub.publish(modifiedMsg);
-        //ROS_INFO("header param name: %s", odom_source.c_str());
+
     }
 
 private:
     ros::NodeHandle n;
     ros::Subscriber sub;
-    std::string odom_source;
     ros::Publisher pub;
 
     dynamic_reconfigure::Server<second_project::parametersConfig> server;
--- src/odom_to_tf.cpp
+++ src/odom_to_tf.cpp
@@ -13,20 +13,14 @@ public:
     void callback(const nav_msgs::Odometry::ConstPtr &msg)
     {
         // Access pose from the Odometry message
-        // ros::NodeHandle nh_private("~");
         tf::Transform transform;
-        //n.getParam("root_frame", root);
-        //nh_private.getParam("child_frame", child);
         root = "odom_frame";
         child = "UGV_odom";
         transform.setOrigin(tf::Vector3(msg->pose.pose.position.x, msg->pose.pose.position.y, msg->pose.pose.position.z));
         transform.setRotation(tf::Quaternion(
             msg->pose.pose.orientation.x, msg->pose.pose.orientation.y, msg->pose.pose.orientation.z, msg->pose.pose.orientation.w));
-        //ros::Time current_time = ros::Time::now();
-        //br.sendTransform(tf::StampedTransform(transform, current_time, root, child));
         br.sendTransform(tf::StampedTransform(transform, msg->header.stamp, root, child));
-        // ROS_INFO("Root param name: %s", root.c_str());
-        // ROS_INFO("Child param name: %s", child.c_str());
+
     }
 
 private:
